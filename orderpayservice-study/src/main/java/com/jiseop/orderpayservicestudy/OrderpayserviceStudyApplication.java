f31d1867b4f3839ac5eca44b77a94f9e5beefd95
jiseop <jiseop992@gmail.com>
2025-05-21 15:31:39 +0900

    프로젝트 생성 및 gitignore 수정

---
+++ orderpayservice-study/src/main/java/com/jiseop/orderpayservicestudy/OrderpayserviceStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jiseop.orderpayservicestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderpayserviceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderpayserviceStudyApplication.class, args);
+    }
+
+}
---
+++ orderpayservice-study/src/test/java/com/jiseop/orderpayservicestudy/OrderpayserviceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiseop.orderpayservicestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderpayserviceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
